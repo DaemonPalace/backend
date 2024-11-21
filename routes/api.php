0cd09ef87dda1451c1fdaa3eca5b2eeed10307f7
Blackjack555y <danieldavidgobr@gmail.com>
2024-11-21 18:45:44 -0500

    ...

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+
 Route::apiResource('/products',ProductController::class);
