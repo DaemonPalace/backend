7940a0fa6fd8123c170905c09de95f6ad9325afb
DaemonPalace <santiagoprtr@unisabana.edu.co>
2024-11-25 13:05:08 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::get('/test', function () {
 
 Route::apiResource('/products',ProductController::class);
 Route::apiResource('/orders',OrderController::class);
-Route::put('/api/orders/{id}/state', [OrderController::class, 'changeState']);
+Route::put('/api/orders/{id}', [OrderController::class, 'changeState']);
